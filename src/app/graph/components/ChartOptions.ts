583b5cad3148b3bcab1379f3c2ce99e2e62ffef3
Rizky saputra <kiki014045@gmail.com>
2024-10-05 10:39:39 +0700

    done fix chart

--- src/app/graph/components/ChartOptions.ts
+++ src/app/graph/components/ChartOptions.ts
@@ -3,13 +3,32 @@ import { ApexOptions } from "apexcharts";
 const chartOptions: ApexOptions = {
   chart: {
     toolbar: { show: false },
-    id: "apexchart-example",
+    id: "apexchart",
   },
   xaxis: {
     type: "datetime",
     tickAmount: 2,
   },
-  yaxis: { opposite: true },
+  yaxis: {
+    opposite: true,
+  },
+
+  dataLabels: {
+    formatter: (val: string | number | number[], opts?: any) => {
+      return val + "%";
+    },
+  },
+  tooltip: {
+    x: {
+      format: "dd-MMMM-yyyy ",
+    },
+    y: {
+      formatter: function (value: any) {
+        return value + "%";
+      },
+    },
+  },
+
   legend: { horizontalAlign: "left" },
 };
 
